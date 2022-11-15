0506522cf814a2ffaa0ab800cd59ee0067312089
Sanjar Saparov <117997805+sanzarr0704@users.noreply.github.com>
2022-11-15 19:06:28 +0600

    Это д/з 4

---
+++ Home_work4.js
@@ -0,0 +1,13 @@
+// Задание №1
+function CurrentMonthInAWord(){
+    for(let i=1;i<=4; i++) {
+        alert("текуший месяц словом!"+1=i);
+        }
+    }
+// Задание №2
+function showtop() {
+    let top = "текуший месяц словом!";
+    alert(top)
+}
+
+console.log(showtop())
